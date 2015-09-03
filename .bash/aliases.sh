42e19fc29ad40ef01eb35929ebf75063fd0acea0
Adam Hartz <ahartz1@gmail.com>
2015-09-03 19:42:52 -0400

    Added Kevin Skogland's and my aliases and environmental variables.

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -1,2 +1,16 @@
 # aliases go here, e.g.
 # alias g='git'
+# CONVENIENCE SHORTCUTS
+alias ll='ls -lahG'
+#alias home='cd ~'
+#alias up='cd ..'
+alias irb='irb --simple-prompt'
+
+# REDEFINING COMMANDS TO AVOID PROBLEMS AND ADD OPTIONS
+alias mv='mv -i'
+alias cp='cp -i'
+alias rm='rm -i'
+alias df='df -h'
+alias du='du -h'
+alias mkdir='mkdir -p'
+alias nano='nano -z'
--- .bash/env.sh
+++ .bash/env.sh
@@ -1,2 +1,17 @@
 # environment variable exports go here, e.g.
 # export ENV_VARIABLE='foo'
+# SETTING ENVIORNMENT VARIABLES
+#export LESS='-M'
+#export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:$PATH"
+
+export HISTSIZE=2000				# 500 is default
+#export HISTTIMEFORMAT='%b %d %I:%M %p '		# using strftime format
+export HISTCONTROL=ignoreboth			# ignoredups:ignorespace
+export HISTIGNORE="history:pwd:exit:df:ls:ls -la:ls -la:ll:ls -a"
+
+# Customizing command line prompt
+#export PS1="\W\n > "
+
+# Specifying options grep should use by default
+export GREP_OPTIONS="--color=auto"
+export GREP_COLOR="7;41"
