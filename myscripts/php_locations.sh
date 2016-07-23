e9fd6abb05bd227756b27bb40cbc70bffb5a91dd
Adam J Hartz <ahartz1@gmail.com>
2016-07-23 12:17:54 -0400

    Add formatting and parameter definitions to description, clean up extra spaces in `echo`s

--- myscripts/php_locations.sh
+++ myscripts/php_locations.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-# By default, this script shows the location of all of the config.ini files on
+################################################################################
+#
+#                               PHP_LOCATIONS         
+#
+# By default, this script shows the location of all of the `config.ini` files on
 # this machine whose path includes the system php version. You can also specify
-# any other file name on the command line when calling the script.
+# any other filename on the command line when calling the script.
 #
-# Folders at the root level that start with 'V' are skipped, this is because I
-# wanted to avoid searching the /Volumes folder.
+# Folders at the root level that start with 'V' are skipped. This is to avoid
+# searching the `/Volumes` folder.
 #
-# Example: `. php_locations.sh xdebug.so`
+# @param string SEARCH_TERM (optional) String that must appear in filename
+#
+# Example call: `. php_locations.sh xdebug.so`
+#
+################################################################################
 
-# Check if we got any command line arguments and set SEARCH_TERM accordingly
+# Check if we got any command-line arguments and set SEARCH_TERM accordingly
 if [ $# -eq 0 ]; then
 	SEARCH_TERM=php.ini
 else
@@ -18,7 +26,7 @@ fi
 
 # Parse the system's php version
 PHP_VERSION=`php --version | sed -E -e 's/^PHP ([0-9.]*) .*/\1/' -e 's/^[CZ].*//'`
-echo -e "\n" System PHP Version: "$PHP_VERSION" "\n"
+echo -e "\n"System PHP Version: "$PHP_VERSION""\n"
 
 echo ----switching to root directory----
 cd /
