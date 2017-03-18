00ae4bd75554c1d64598c4970182efaf7a9710a8
Adam Hartz <ahartz1@gmail.com>
2017-03-17 22:15:18 -0400

    Make the `--color=auto` flag specific to Linux

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -6,9 +6,16 @@ alias darkbkgnd="export GITC='tput setaf 251'"
 alias gitlog='git log --graph --oneline --decorate --all'
 alias irb='irb --simple-prompt'
 alias lightbkgnd="export GITC='tput setaf 239'"
-alias ll='ls -lAhGF --color=auto'
+
+if [ `uname | grep Linux` &> /dev/null ]; then
+	alias ll='ls -lAhGF --color=auto'
+	alias lls='ls -FGA1 --color=auto'
+else
+	alias ll='ls -lAhGF'
+	alias lls='ls -FGA1'
+fi
+
 alias lll='ls -lAhGF | less'
-alias lls='ls -FGA1 --color=auto'
 alias turnoffipv6="networksetup -setv6off Wi-Fi"
 alias turnonipv6="networksetup -setv6automatic Wi-Fi"
 
