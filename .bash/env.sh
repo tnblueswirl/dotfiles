8b25751a81d0b621dbb90706892f36b78125a6bf
Adam J Hartz <ahartz1@gmail.com>
2018-08-11 11:25:00 -0400

    Support for PaperColor, partial fix for solarized in Apple Terminal

--- .bash/env.sh
+++ .bash/env.sh
@@ -3,15 +3,15 @@
 
 # SETTING ENVIORNMENT VARIABLES
 # export LESS='-M'
-#export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:$PATH"
+# NOTE: PATH is altered elsewhere, updating here could result in duplications)
 
-# Customizing command line prompt
-#export PS1="\W\n > "
-
-# Specifying options grep should use by default
-# export GREP_COLOR="7;41"
+# Let bash know we want to use Vim for everything!
 EDITOR="vim"
 export EDITOR
+
+# If you don't like solarized (or if it is not displaying correctly)...
+# THEME="PaperColor"
+# export THEME
 
 if [ -f ~/Library/Preferences/WebIde100/phpstorm.vmoptions ]; then
   WEBIDE_VM_OPTIONS=$HOME/Library/Preferences/WebIde100/phpstorm.vmoptions
