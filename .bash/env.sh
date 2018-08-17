39425a54ba6df665b6c700014283f1cde5c18c68
Adam J Hartz <ahartz1@gmail.com>
2018-08-17 09:28:41 -0400

    Set non-solarized light colors if Apple_Terminal and no `$ITERM_PROFILE`

--- .bash/env.sh
+++ .bash/env.sh
@@ -5,6 +5,14 @@
 # export LESS='-M'
 # NOTE: PATH is altered elsewhere, updating here could result in duplications)
 
+if [[ -z "$ITERM_PROFILE" ]] && \
+  [[ "$TERM_PROGRAM" == "Apple_Terminal" ]]
+then
+  # This file is processed after aliases.sh, `solarlight` is defined there
+  solarlight
+fi
+
+
 # Let bash know we want to use Vim for everything!
 EDITOR="vim"
 export EDITOR
