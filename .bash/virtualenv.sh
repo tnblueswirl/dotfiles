75d821c054aa84cae7d7a7d34db43a9c52c0eb77
Adam J Hartz <ahartz1@gmail.com>
2018-09-26 09:23:03 -0400

    Finally get rid of warning about virtualenvwrapper.

--- .bash/virtualenv.sh
+++ .bash/virtualenv.sh
@@ -12,6 +12,4 @@ export PIP_RESPECT_VIRTUALENV=true
 
 if [[ -r /usr/local/bin/virtualenvwrapper.sh ]]; then
   source /usr/local/bin/virtualenvwrapper.sh
-else
-  echo "WARNING: Can't find virtualenvwrapper.sh"
 fi
