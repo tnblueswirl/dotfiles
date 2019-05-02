71d94fb69270930718aabe713621c7f8bc7b20e3
Adam J Hartz <ahartz1@gmail.com>
2019-05-02 09:49:36 -0400

    Symlink the editorconfig files necessary for it to work; gitignore them

--- .bash/vim.sh
+++ .bash/vim.sh
@@ -10,3 +10,19 @@ then
   ln -s ~/.dotfiles/myscripts/yank_path.vim \
         ~/.dotfiles/.vim/bundle/nerdtree/nerdtree_plugin/yank_path.vim
 fi
+
+# Ensure that editorconfig can work
+if [ -d ~/.dotfiles/.vim/bundle/editorconfig-vim ]; then
+  if [ ! -e ~/.dotfiles/.vim/plugin/editorconfig.vim ]; then
+    ln -s ~/.dotfiles/.vim/bundle/editorconfig-vim/plugin/editorconfig.vim \
+          ~/.dotfiles/.vim/plugin/editorconfig.vim
+  fi
+  if [ ! -e ~/.dotfiles/.vim/plugin/editorconfig.py ]; then
+    ln -s ~/.dotfiles/.vim/bundle/editorconfig-vim/plugin/editorconfig.py \
+          ~/.dotfiles/.vim/plugin/editorconfig.py
+  fi
+  if [ ! -e ~/.dotfiles/.vim/autoload/editorconfig.vim ]; then
+    ln -s ~/.dotfiles/.vim/bundle/editorconfig-vim/autoload/editorconfig.vim \
+          ~/.dotfiles/.vim/autoload/editorconfig.vim
+  fi
+fi
