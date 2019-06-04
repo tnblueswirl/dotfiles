c8ae678a24683200e0c8e6aa7b3e72c9a4743dd4
Adam J Hartz <ahartz1@gmail.com>
2019-06-04 09:42:32 -0400

    Explicitly check that local script dependencies are symbolic links

--- .bash/vim.sh
+++ .bash/vim.sh
@@ -5,7 +5,7 @@
 # Solution? Symlink it in if it doesn't already exist.
 if [ -d ~/.dotfiles/.vim/bundle/nerdtree/nerdtree_plugin ] && \
    [ -e ~/.dotfiles/myscripts/yank_path.vim ] && \
-   [ ! -e ~/.dotfiles/.vim/bundle/nerdtree/nerdtree_plugin/yank_path.vim ]
+   [ ! -L ~/.dotfiles/.vim/bundle/nerdtree/nerdtree_plugin/yank_path.vim ]
 then
   ln -s ~/.dotfiles/myscripts/yank_path.vim \
         ~/.dotfiles/.vim/bundle/nerdtree/nerdtree_plugin/yank_path.vim
@@ -13,15 +13,15 @@ fi
 
 # Ensure that editorconfig can work
 if [ -d ~/.dotfiles/.vim/bundle/editorconfig-vim ]; then
-  if [ ! -e ~/.dotfiles/.vim/plugin/editorconfig.vim ]; then
+  if [ ! -L ~/.dotfiles/.vim/plugin/editorconfig.vim ]; then
     ln -s ~/.dotfiles/.vim/bundle/editorconfig-vim/plugin/editorconfig.vim \
           ~/.dotfiles/.vim/plugin/editorconfig.vim
   fi
-  if [ ! -e ~/.dotfiles/.vim/plugin/editorconfig.py ]; then
+  if [ ! -L ~/.dotfiles/.vim/plugin/editorconfig.py ]; then
     ln -s ~/.dotfiles/.vim/bundle/editorconfig-vim/plugin/editorconfig.py \
           ~/.dotfiles/.vim/plugin/editorconfig.py
   fi
-  if [ ! -e ~/.dotfiles/.vim/autoload/editorconfig.vim ]; then
+  if [ ! -L ~/.dotfiles/.vim/autoload/editorconfig.vim ]; then
     ln -s ~/.dotfiles/.vim/bundle/editorconfig-vim/autoload/editorconfig.vim \
           ~/.dotfiles/.vim/autoload/editorconfig.vim
   fi
