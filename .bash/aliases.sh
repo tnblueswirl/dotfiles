b3e2115052e29d24299e221d5e305a03ca0591ed
Adam J Hartz <ahartz1@gmail.com>
2021-02-09 11:38:22 -0500

    update qwork and qlisten aliases to specify 10-minute timeout

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -55,8 +55,8 @@ fi
 alias artisan='php artisan'
 alias composer='COMPOSER_MEMORY_LIMIT=-1 composer'
 alias npmprodbuild='NODE_ENV=production NODE_OPTIONS="--max-old-space-size=4096" npm run build'
-alias qlisten='php artisan queue:listen --tries=1'
-alias qwork='php artisan queue:work --tries=1'
+alias qlisten='php artisan queue:listen --tries=1 --timeout=600'
+alias qwork='php artisan queue:work --tries=1 --timeout=600'
 alias showlinks="ls -lA node_modules/ | grep '^l' | sed -l 's/.*\/\(.*\)$/\1/'"
 alias tinker='php artisan tinker'
 alias updateapp='rm -rf node_modules package-lock.json && composer update cccisd/*'
