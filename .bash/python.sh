c07065f792cd0746c670afd6d671232f31b1ff50
Adam J Hartz <ahartz1@gmail.com>
2021-07-07 09:16:38 -0400

    if pyenv and its directory exists, set its env vars and invoke

--- .bash/python.sh
+++ .bash/python.sh
@@ -2,3 +2,9 @@ if [[ -r $HOME/.pythonrc ]]; then
   PYTHONSTARTUP=$HOME/.pythonrc
   export PYTHONSTARTUP
 fi
+
+if [ -d "$HOME/.pyenv" ] && [ $(which pyenv 2> /dev/null) ]; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init --path)"
+fi
