2a8e505ce5300c11bdf3ea2607c078c1d5cf68b4
Adam J Hartz <ahartz1@gmail.com>
2025-02-01 15:11:12 -0500

    update autocmds, lazy-lock

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,8 +1,4 @@
 -- Markdown-specific settings
-vim.opt_local.expandtab = true
-vim.opt_local.shiftwidth = 2
-vim.opt_local.softtabstop = 2
-vim.opt_local.tabstop = 2
 vim.opt_local.textwidth = 88
 vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
@@ -13,4 +9,5 @@ vim.opt_local.spelllang = "en_us"
 
 -- Add common markdown file extensions to suffixesadd
 vim.opt_local.suffixesadd:append(".md")
+vim.opt_local.suffixesadd:append(".mdc")
 vim.opt_local.suffixesadd:append(".markdown")
--- .config/nvim/after/ftplugin/sql.lua
+++ .config/nvim/after/ftplugin/sql.lua
@@ -1,8 +1,4 @@
 -- SQL-specific settings
-vim.opt_local.expandtab = true
-vim.opt_local.shiftwidth = 4
-vim.opt_local.softtabstop = 4
-vim.opt_local.tabstop = 4
 vim.opt_local.textwidth = 88
 
 -- SQL formatting options
